98b0f62c33a6c336bb4997aa04babe6b97a64f97
TemmaZ <temazavod@gmail.com>
2018-02-15 19:06:55 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ class Main
 {
 	public static void main(String... strings)
 	{
+		System.out.println(Main.class);
 		int count = 50;
 		String text = "Hello World";
 		for(int index = 0; index < count; ++index)
