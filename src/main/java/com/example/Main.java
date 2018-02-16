c3162e35fe7e2cc151c1c4c3334071afda194631
TemmaZ <temazavod@gmail.com>
2018-02-16 12:18:04 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,11 +5,11 @@ class Main
 	public static void main(String... strings)
 	{
 		System.out.println(Main.class);
-		int count = 50;
+		int count = Integer.valueOf(strings[0]);
 		String text = "Hello World";
-		for(int index = 0; index < count; ++index)
+		for (int index = 0; index < count; ++index)
 		{
-			System.out.println(String.format("%d - %s", index, text));
+			System.out.println(String.format("%d - %s - %s", index, text, strings[1]));
 		}
 	}
 }
